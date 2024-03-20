5bb1ae282c7ff8695f5946fe3dd9cda720d09f72
Ayyub Ansari <78730656+AyyubAnsari786@users.noreply.github.com>
2024-03-20 12:21:41 +0530

    contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,38 +1,33 @@
 <?php
-  /**
-  * Requires the "PHP Email Form" library
-  * The "PHP Email Form" library is available only in the pro version of the template
-  * The library should be uploaded to: vendor/php-email-form/php-email-form.php
-  * For more info and help: https://bootstrapmade.com/php-email-form/
-  */
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'iamayyubansari@gmail.com';
+require 'vendor/autoload.php';
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$mail = new PHPMailer(true);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+try {
+    //Server settings
+    $mail->isSMTP();
+    $mail->Host = 'smtp-relay.brevo.com';
+    $mail->Port = 587;
+    $mail->SMTPAuth = true;
+    $mail->Username = 'iamayyubansari@gmail.com';
+    $mail->Password = 'tEVLnyhZI23wO8q1';
+    $mail->SMTPSecure = 'tls';
 
- // Uncomment below code if you want to use SMTP to send emails. You need to enter your correct SMTP credentials.
-  $contact->smtp = array(
-    'host' => 'smtp-relay.brevo.com',
-    'username' => 'iamayyubansari@gmail.com',
-    'password' => 'tEVLnyhZI23wO8q1',
-    'port' => '587'
-  );
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
+    //Recipients
+    $mail->setFrom('iamayyubansari@gmail.com', 'Your Name');
+    $mail->addAddress('recipient@example.com', 'Recipient Name');
 
-  echo $contact->send();
+    //Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Test Email via Brevo';
+    $mail->Body = 'This is a test email sent via Brevo SMTP';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
